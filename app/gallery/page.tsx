75411ef9864748e960b2fdd84b01c6e5f8f5b6f8
dimitri.anoudis <dimitri.anoudis@news.co.uk>
2025-12-12 01:00:32 +0100

    fix: fixes pnpm import

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -4,9 +4,9 @@ import Gallery from "@/components/Gallery";
 
 export default async function GalleryPage() {
   const session = await getSession();
+  if (!session.isLoggedIn) redirect("/login");
   const role = session.user?.role || 'member';
 
-  if (!session.isLoggedIn) redirect("/login");
 
   return (
     <main className="container section">
